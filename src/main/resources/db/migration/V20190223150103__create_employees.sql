6d9e813e6d7b2614f3cc42c5c9bafd7c9a31c640
adelsonarmindo <adelsonarmindo-@hotmail.com>
2019-03-09 22:35:01 -0600

    Finished UC1, UC2

--- src/main/resources/db/migration/V20190223150103__create_employees.sql
+++ src/main/resources/db/migration/V20190223150103__create_employees.sql
@@ -1,8 +1,38 @@
 create table EMPLOYEE
+
 (
   ID SERIAL NOT NULL
     primary key,
   LAST_NAME VARCHAR(24) not null,
-  first_name varchar(24) not null
+  first_name varchar(24) not null,
+  email varchar (24) not null,
+  phone_number integer (12)
+
+
 );
 
+create table JOB
+
+(
+  ID SERIAL NOT NULL
+    primary key,
+  LAST_NAME VARCHAR(24) not null,
+  first_name varchar(24) not null,
+  email varchar (24) not null,
+  phone_number integer (12)
+
+
+);
+
+create table CUSTOMER
+
+(
+  ID SERIAL NOT NULL
+    primary key,
+  LAST_NAME VARCHAR(24) not null,
+  first_name varchar(24) not null,
+  email varchar (24) not null,
+  phone_number integer (12)
+
+
+);
